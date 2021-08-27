92d9364b875c77dcd8b4655f505912afd7f05ff2
alputv <77716705+alputv@users.noreply.github.com>
2021-08-27 20:41:31 +0000

    Deploying to gh-pages from @ UpDevsDB/updevsdb.github.io@aa280e2e44b7caa44aaa162f64bcd07ecdb66fb0 🚀

---
+++ assets/Spinner.7403791c.js
@@ -0,0 +1,1 @@
+import{o as e,b as s,e as r}from"./vendor.79db57bd.js";const a={},o={class:"relative h-10 w-10 my-60 mx-auto"},d=r("div",{class:"cube1 bg-discord-green-560 h-4 w-4 absolute"},null,-1),l=r("div",{class:"cube2 bg-discord-green-560 h-4 w-4 absolute"},null,-1);a.render=function(r,a){return e(),s("div",o,[d,l])};var n=a;export{n as S};
